dfc677b9702f3fa294853dcbdf9256204375d66a
kbprophecy21 <djprophecy21@gmail.com>
2025-10-10 00:14:29 -0500

    working on location.java right now

--- envy/app/src/main/java/io/kyle/inventory/domain/inventory/Location.java
+++ envy/app/src/main/java/io/kyle/inventory/domain/inventory/Location.java
@@ -2,4 +2,10 @@ package io.kyle.inventory.domain.inventory;
 
 public class Location {
 
+    private String bayArea;
+    private String selfLocation;
+    private String department;
+    private char section;
+    
+
 }
